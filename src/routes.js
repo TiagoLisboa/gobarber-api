5a1dcd98febe02451952dc0ffdb2582c7efd4c3c
Tiago Lisboa <tiago.caio.ol@gmail.com>
2020-02-13 19:30:27 -0300

    user model definition

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import Router from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Tiago Lisboa',
+    email: 'tiago.caio.ol@gmail.com',
+    password_hash: '12345678',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
