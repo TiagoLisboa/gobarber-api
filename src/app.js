09001e2d229c4a5401eab866265b133046d77def
Tiago Lisboa <tiago.caio.ol@gmail.com>
2020-02-15 12:13:47 -0300

    listing providers
    also url attribute on file

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import routes from './routes';
+import path from 'path';
 
 import './database';
 
@@ -13,6 +14,10 @@ class App {
 
   middlewares() {
     this.server.use(express.json());
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
   }
 
   routes() {
@@ -21,4 +26,3 @@ class App {
 }
 
 export default new App().server;
-
