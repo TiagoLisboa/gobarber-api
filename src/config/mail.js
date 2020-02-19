e4b23f9eef22c71f3bf01de6e83c88c8a391bdc8
Tiago Lisboa <tiago.caio.ol@gmail.com>
2020-02-18 21:12:25 -0300

    cancellation email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '318ea3a07b4a0f',
+    pass: '592c025be4bdf7',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
